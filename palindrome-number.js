6d5edb6024ca36e96b2389e7b113eea58d3a9856
Joan Gavelán <joangavelan@gmail.com>
2021-10-08 04:23:04 -0500

    solved

--- palindrome-number.js
+++ palindrome-number.js
@@ -2,7 +2,7 @@ const isPalindrome = (n) => reverse(n) === n;
 
 function reverse(n) {
   if(n >= 0) {
-    var temp = 0;
+    let temp = 0;
     while (n) {
       temp *= 10;
       temp += n % 10;
