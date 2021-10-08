01222c8bd076674c2577d3e225322605e64ab1cb
Joan Gavelán <joangavelan@gmail.com>
2021-10-08 04:18:50 -0500

    solved

---
+++ palindrome-number.js
@@ -0,0 +1,16 @@
+const isPalindrome = (n) => reverse(n) === n;
+
+function reverse(n) {
+  if(n >= 0) {
+    var temp = 0;
+    while (n) {
+      temp *= 10;
+      temp += n % 10;
+      n = Math.floor(n / 10);
+    }
+    return temp;
+  }
+  return false;
+}
+
+const num = 10;
